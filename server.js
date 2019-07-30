cf50f66a49b5761096b603813ca636f8f703f74d
ChungPhamVan <phamvanchung11111999@gmail.com>
2019-07-30 12:08:00 +0700

    'commit-1'

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 var express = require('express');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.use('/', indexRoute);
+
+var listUser = [];
+var port = 3000;
+
+server.listen(process.env.PORT || port, function() {
+    console.log('Server listening..., port: ' + port);
+});
