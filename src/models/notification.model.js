3575259ecb6e9d55f969510947bb56912ba4de58
ChungPhamVan <phamvanchung11111999@gmail.com>
2019-09-02 12:53:07 +0700

    'count_notifications_unread_and_style_for_viewModal'

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -26,6 +26,14 @@ NotificationSchema.statics = {
     return this.find({
       "receiverId": userId
     }).sort({ "createAt": -1 }).limit(limit).exec();
+  },
+  countNotifUnread(userId) {
+    return this.count({
+      $and: [
+        {"receiverId": userId},
+        {"isRead": false}
+      ]
+    }).exec();
   }
 }
 
@@ -37,16 +45,16 @@ const NOTIFICATION_CONTENTS = {
   getContent: (notificationType, isRead, userId, userName, userAvatar) => {
     if(notificationType === NOTIFICATION_TYPES.ADD_CONTACT) {
       if(!isRead) {
-        return `<span class="notif-readed-false" data-uid="${ userId }">
+        return `<div class="notif-readed-false" data-uid="${ userId }">
                 <img class="avatar-small" src="images/users/${ userAvatar }" alt=""> 
                 <strong>${ userName }</strong> đã gửi cho bạn một lời mời kết bạn!
-              </span><br><br><br>`;
+              </div>`;
       }
 
-      return `<span data-uid="${ userId }">
+      return `<div data-uid="${ userId }">
                 <img class="avatar-small" src="images/users/${ userAvatar }" alt=""> 
                 <strong>${ userName }</strong> đã gửi cho bạn một lời mời kết bạn!
-              </span><br><br><br>`;
+              </div>`;
     }
 
     return "No matching with  any  notification type";
--- src/public/js/removeRequestContact.js
+++ src/public/js/removeRequestContact.js
@@ -21,7 +21,8 @@ function removeRequestContact() {
 }
 
 socket.on('response-remove-request-contact', function(user) {
-  $('.noti_content').find(`span[data-uid = ${user.id}]`).remove();
+  $('.noti_content').find(`div[data-uid = ${user.id}]`).remove();
+  $('ul.list-notifications').find( `li>div[data-uid = ${user.id}]`).parent().remove();
   deIncreaseNumberNotifContact('count-request-contact-received');
 
   deIncreaseNumberNotification('noti_contact_counter');
