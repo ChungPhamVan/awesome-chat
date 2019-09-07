0f9122d57f96a9e042daa5f691b5c4c8466d5890
ChungPhamVan <phamvanchung11111999@gmail.com>
2019-09-08 01:07:42 +0700

    'render_image_and_file_format_message_data'

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -24,6 +24,7 @@ let getHome = async (req, res, next) => {
 
   let allConversationWithMessages = getAllConversationItems.allConversationWithMessages;
 
+  
   return res.render('main/home/home', {
     errors: req.flash('errors'),
     success: req.flash('success'), 
