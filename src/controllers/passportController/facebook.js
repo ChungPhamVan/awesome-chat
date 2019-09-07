acd886532e7646f287df2f90ba6794dd53c0e2e0
ChungPhamVan <phamvanchung11111999@gmail.com>
2019-09-08 01:09:04 +0700

    'render_image_and_file_format_message_data'

--- src/controllers/passportController/facebook.js
+++ src/controllers/passportController/facebook.js
@@ -43,7 +43,7 @@ let initPassportFacebook = () => {
     done(null, user._id);
   });
   passport.deserializeUser((id, done) => {
-    UserModel.findUserById(id)
+    UserModel.findUserByIdForSessionToUse(id)
       .then(user => {
         return done(null, user);
       })
